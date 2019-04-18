8634e1f7dfb161e570724b9df75222ddc7d66185
Maecenas Li <lx70716@gmail.com>
2019-04-19 08:37:52 +0900

    ✨ Q03_DuplicationInArray

--- src/main/java/PointAtOffer/Q03_DuplicationInArray.java
+++ src/main/java/PointAtOffer/Q03_DuplicationInArray.java
@@ -21,4 +21,27 @@ public class Q03_DuplicationInArray {
         }
         return -1;
     }
+
+    /**
+     * Copy nums[i] to nums[nums[i]], if already present then is duplicate
+     */
+    public static int getDuplication2(int[] nums) {
+        if (nums == null || nums.length <= 0) return -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] != i) {
+                if (nums[i] == nums[nums[i]]) {
+                    return nums[i];
+                }
+                swap(nums, i, nums[i]);
+            }
+        }
+        return -1;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
 }
