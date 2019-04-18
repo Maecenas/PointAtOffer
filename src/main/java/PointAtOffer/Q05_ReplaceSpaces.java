992ecc23611d4ac4aae4498238e9010d9a89e5ca
Maecenas Li <lx70716@gmail.com>
2019-04-19 08:42:47 +0900

    ✨ Q05_ReplaceSpaces

---
+++ src/main/java/PointAtOffer/Q05_ReplaceSpaces.java
@@ -0,0 +1,24 @@
+package PointAtOffer;
+
+public class Q05_ReplaceSpaces {
+
+    public static String replaceBlank(StringBuilder str) {
+        int P1 = str.length() - 1;
+        for (int i = 0; i <= P1; i++)
+            if (str.charAt(i) == ' ')
+                str.append("  ");
+
+        int P2 = str.length() - 1;
+        while (P1 >= 0 && P2 > P1) {
+            char c = str.charAt(P1--);
+            if (c == ' ') {
+                str.setCharAt(P2--, '0');
+                str.setCharAt(P2--, '2');
+                str.setCharAt(P2--, '%');
+            } else {
+                str.setCharAt(P2--, c);
+            }
+        }
+        return str.toString();
+    }
+}
