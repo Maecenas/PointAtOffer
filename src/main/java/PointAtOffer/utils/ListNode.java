071041c3361bfe97ceab0b4dbb942b01c3022e41
Maecenas Li <lx70716@gmail.com>
2019-04-19 09:05:21 +0900

    ✨ ListNode

---
+++ src/main/java/PointAtOffer/utils/ListNode.java
@@ -0,0 +1,29 @@
+package PointAtOffer.utils;
+
+public class ListNode<T> {
+
+    public T val;
+    public ListNode<T> next;
+
+    public ListNode(T val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder ret = new StringBuilder();
+        ret.append("[");
+        for (ListNode cur = this; ; cur = cur.next) {
+            if (cur == null) {
+                ret.deleteCharAt(ret.lastIndexOf(" "));
+                ret.deleteCharAt(ret.lastIndexOf(","));
+                break;
+            }
+            ret.append(cur.val);
+            ret.append(", ");
+        }
+        ret.append("]");
+        return ret.toString();
+    }
+}
