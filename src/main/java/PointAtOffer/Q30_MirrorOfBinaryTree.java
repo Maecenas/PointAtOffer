5113d0d800de622bff8e16d694d6bf8b5c25f20d
Maecenas Li <lx70716@gmail.com>
2019-04-23 22:44:56 +0900

    ✨ Q30_MirrorOfBinaryTree

--- src/main/java/PointAtOffer/Q30_MirrorOfBinaryTree.java
+++ src/main/java/PointAtOffer/Q30_MirrorOfBinaryTree.java
@@ -2,15 +2,38 @@ package PointAtOffer;
 
 import PointAtOffer.utils.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Q30_MirrorOfBinaryTree {
 
-    public static void mirror(TreeNode root) {
+    public static void mirrorRecursively(TreeNode root) {
         if (root == null || root.left == null || root.right == null) return;
 
-        TreeNode tmp = root.left;
-        root.left = root.right;
-        root.right = tmp;
-        mirror(root.left);
-        mirror(root.right);
+        swap(root);
+        mirrorRecursively(root.left);
+        mirrorRecursively(root.right);
+    }
+
+    /**
+     * See also {@code Q07_TraversalOfBinaryTree.levelorder()}
+     */
+    public static void mirrorIteratively(TreeNode<Integer> root) {
+        if (root == null) return;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode curr = q.poll();
+            swap(curr);
+            if (curr.left != null) q.offer(curr.left);
+            if (curr.right != null) q.offer(curr.right);
+        }
+    }
+
+    private static void swap(TreeNode node) {
+        TreeNode tmp = node.left;
+        node.left = node.right;
+        node.right = tmp;
     }
 }
