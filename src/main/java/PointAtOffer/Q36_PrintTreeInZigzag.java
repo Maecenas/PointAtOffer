49ed630415dd68908546c87bb07b627159c35110
Maecenas Li <lx70716@gmail.com>
2019-04-24 15:28:06 +0900

    ✨ Q36_PrintTreeInZigzag

---
+++ src/main/java/PointAtOffer/Q36_PrintTreeInZigzag.java
@@ -0,0 +1,32 @@
+package PointAtOffer;
+
+import PointAtOffer.utils.TreeNode;
+
+import java.util.*;
+
+public class Q36_PrintTreeInZigzag {
+
+    public static List<List<?>> print(TreeNode<?> root) {
+        if (root == null) return new ArrayList<>();
+
+        List<List<?>> res = new ArrayList<>();
+        Queue<TreeNode<?>> q = new LinkedList<>();
+        q.add(root);
+        boolean needReverse = false;
+        TreeNode<?> node;
+        while (!q.isEmpty()) {
+            List<Object> list = new ArrayList<>();
+            for (int size = q.size(); size > 0; size--) {
+                node = q.poll();
+                if (node == null) continue;
+                list.add(node.val);
+                q.offer(node.left);
+                q.offer(node.right);
+            }
+            if (needReverse) Collections.reverse(list);
+            needReverse = !needReverse;
+            if (!list.isEmpty()) res.add(list);
+        }
+        return res;
+    }
+}
