a52be793d04d30b824eefac3c8a56d8a1c926eb6
Maecenas Li <lx70716@gmail.com>
2019-04-27 20:16:51 +0900

    ✨ Q46_KLeastNumbers

--- src/main/java/PointAtOffer/Q46_KLeastNumbers.java
+++ src/main/java/PointAtOffer/Q46_KLeastNumbers.java
@@ -24,4 +24,78 @@ public class Q46_KLeastNumbers {
         }
         return new ArrayList<>(maxHeap);
     }
+
+    /**
+     * Partition implementation that would modify the original Array
+     * O(n), O(1)
+     */
+    public static List<Integer> getLeastNumbersPartition(int[] nums, int k) {
+        if (nums == null || k > nums.length || k <= 0) return new ArrayList<>();
+
+        getKLeastNumbers(nums, k - 1);
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            res.add(nums[i]);
+        }
+        return res;
+    }
+
+    /**
+     * Iteratively select element to partition the Array,
+     * if happens to make the first k element of nums less than v, return
+     */
+    private static void getKLeastNumbers(int[] nums, int k) {
+        int lo = 0, hi = nums.length - 1, j;
+        while (lo < hi) {
+            j = partition(nums, lo, hi);
+            if (j == k) return;
+            if (j > k) {
+                hi = j - 1;
+            } else {
+                lo = j + 1;
+            }
+        }
+    }
+
+    /**
+     * partition the subarray nums[lo..hi] so that a[lo..j-1] <= a[j] <= a[j+1..hi]
+     * and return the index j.
+     */
+    private static int partition(int[] nums, int lo, int hi) {
+        int v = nums[lo], i = lo, j = hi + 1;
+
+        // nums[lo] is unique largest element
+        while (nums[++i] < v) {
+            if (i == hi) {
+                swap(nums, lo, hi);
+                return hi;
+            }
+        }
+
+        // nums[lo] is unique smallest element
+        while (v < nums[--j]) {
+            if (j == lo + 1) {
+                return lo;
+            }
+        }
+
+        // the main loop
+        while (i < j) {
+            swap(nums, i, j);
+            while (i < hi - 1 && nums[++i] < v) ;
+            while (j > lo && nums[--j] > v) ;
+        }
+
+        // put partitioning item v at nums[j]
+        swap(nums, lo, j);
+
+        // now, nums[lo .. j-1] <= nums[j] <= nums[j+1 .. hi]
+        return j;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
 }
