a870b983facef8c8bb34ef77e1d6489e12f22c46
Maecenas Li <lx70716@gmail.com>
2019-05-02 22:04:54 +0900

    🎨 Q52_TranslateNumbersToStrings

--- src/main/java/PointAtOffer/Q52_TranslateNumbersToStrings.java
+++ src/main/java/PointAtOffer/Q52_TranslateNumbersToStrings.java
@@ -2,6 +2,9 @@ package PointAtOffer;
 
 public class Q52_TranslateNumbersToStrings {
 
+    /**
+     * 'A' -> 1 and is different from the book as LeetCode #91
+     */
     public static int getTranslationCount(String s) {
         if (s == null || s.length() == 0) return 0;
 
@@ -17,16 +20,21 @@ public class Q52_TranslateNumbersToStrings {
             if (oneDigit != 0) {
                 dp[i] += dp[i - 1];
             }
-            if (s.charAt(i - 2) == '0') {
-                continue;
-            }
-            // Since JDK 9
-            // twoDigit = Integer.parseInt(s, i - 2, i, 10);
-            twoDigit = Integer.valueOf(s.substring(i - 2, i));
-            if (twoDigit <= 26) {
-                dp[i] += dp[i - 2];
+            if (s.charAt(i - 2) != '0') {
+                // Since JDK 9
+                // twoDigit = Integer.parseInt(s, i - 2, i, 10);
+                twoDigit = Integer.valueOf(s.substring(i - 2, i));
+                if (twoDigit <= 26) {
+                    dp[i] += dp[i - 2];
+                }
             }
         }
         return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getTranslationCount("10"));  //2
+//        System.out.println(getTranslationCount("1234"));  //3
+//        System.out.println(getTranslationCount("12258")); //5
     }
 }
