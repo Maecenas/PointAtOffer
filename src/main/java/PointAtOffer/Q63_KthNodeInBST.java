26d30be40a735b029e228fe64439170c69fdf6ee
Maecenas Li <lx70716@gmail.com>
2019-05-04 16:15:34 +0900

    🎨 Q63_KthNodeInBST

--- src/main/java/PointAtOffer/Q63_KthNodeInBST.java
+++ src/main/java/PointAtOffer/Q63_KthNodeInBST.java
@@ -2,27 +2,54 @@ package PointAtOffer;
 
 import PointAtOffer.utils.TreeNode;
 
+import java.util.Stack;
+
 public class Q63_KthNodeInBST {
 
     private static TreeNode ret;
     private static int cnt;
 
     public static TreeNode kthNode(TreeNode root, int k) {
+        if (root == null || k <= 0) return null;
+
         cnt = 0;
-        ret = null;
-        inOrder(root, k);
-        return ret;
+        // Recursive method calls
+        // ret = null;
+        // inOrderRecursively(root, k);
+        // return ret;
+        return inOrderIteratively(root, k);
     }
 
-    private static void inOrder(TreeNode root, int k) {
+    private static void inOrderRecursively(TreeNode root, int k) {
         if (root == null || cnt >= k) return;
 
-        inOrder(root.left, k);
+        inOrderRecursively(root.left, k);
         cnt++;
         if (cnt == k) {
             ret = root;
             return;
         }
-        inOrder(root.right, k);
+        inOrderRecursively(root.right, k);
+    }
+
+    private static TreeNode inOrderIteratively(TreeNode root, int k) {
+        if (root == null || cnt >= k) return null;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+            if (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                curr = stack.pop();
+                cnt++;
+                if (cnt == k) {
+                    return curr;
+                }
+                curr = curr.right;
+            }
+        }
+        return null;
     }
 }
