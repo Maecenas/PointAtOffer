ed045e333411f4867a8c825b7816d895b2e23a3f
Maecenas Li <lx70716@gmail.com>
2019-05-05 17:26:41 +0900

    🎨 Q69_ContinuousSequenceWithSum

--- src/main/java/PointAtOffer/Q69_ContinuousSequenceWithSum.java
+++ src/main/java/PointAtOffer/Q69_ContinuousSequenceWithSum.java
@@ -31,4 +31,45 @@ public class Q69_ContinuousSequenceWithSum {
         }
         return res;
     }
+
+    /**
+     * Expand sequence around center at each 0.5 increment
+     */
+    public static List<List<Integer>> findContinuousSequence2(int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (sum < 3) return res;
+
+        int mid = sum;
+        // multiply sum by 2 to avoid float calculation
+        sum <<= 1;
+        while (mid > 0) {
+            if (sum % mid == 0) {
+                int div = sum / mid;
+                if ((div & 0x1) == 1) {
+                    // div is odd
+                    int n = (mid >> 1) - (div >> 1);
+                    if (n > 0) {
+                        ArrayList<Integer> list = new ArrayList<>();
+                        while (n <= (mid >> 1) + (div >> 1)) {
+                            list.add(n++);
+                        }
+                        res.add(list);
+                    }
+                } else {
+                    // div is even
+                    int n = (mid >> 1) - ((div - 1) >> 1);
+                    if (n > 0 && ((mid & 0x1) == 0x1)) {
+                        ArrayList<Integer> list = new ArrayList<>();
+                        while (n <= (mid >> 1) + (div >> 1)) {
+                            list.add(n++);
+                        }
+                        res.add(list);
+                    }
+                }
+            }
+            mid--;
+        }
+        // Collections.reverse(res);
+        return res;
+    }
 }
